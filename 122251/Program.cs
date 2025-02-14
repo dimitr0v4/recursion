58260895af1d2fbee09e9c27ec616ac3fa806402
MEGA <MEGA@gmail.com>
2025-02-14 15:24:12 +0200

    fixed it ayyyy

--- 122251/Program.cs
+++ 122251/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine($"the sum of its digits is equal to {DigitSum(n)}");
 
         }
-        public int DigitSum(int n)
+        public static int DigitSum(int n)
         { if (n < 10) { return n; }
             else { return (n % 10 + DigitSum(n / 10)); } }
     }
