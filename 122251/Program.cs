fabd18cb711123d490aaf55eca9d40895c81087a
Nikito69 <nikiniki2777@gmail.com>
2025-02-14 14:58:38 +0200

    Add project files.

---
+++ 122251/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _122251
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+      int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("the sum of its digits is equal to "+DigitSum(n));
+
+        }
+        public int DigitSum(int n)
+        { if (n < 10) { return n; }
+            else { return (n % 10 + DigitSum(n / 10)); } }
+    }
+}
